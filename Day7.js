967051edf06db4c3a6aa92e13c40c6c77d4ecddf
Harsh-Gupta-1111 <hg902756@gmail.com>
2025-05-20 12:19:58 +0530

    Add files via upload

---
+++ Day7.js
@@ -0,0 +1,28 @@
+const obj={
+    name:"Harsh",
+    city:"meerut",
+    age: 20
+}
+console.log(obj.name)
+const obj2=new Object();
+const obj3={}
+
+console.log(obj3)
+
+//add in object
+obj2.college="Abes";
+obj2.contect=9027568556;
+
+
+// delete in object
+delete obj.city;
+// console.log(obj);
+
+
+// console.log(obj2);
+
+
+
+const A="city";
+obj[A]="meerut";
+console.log(obj)
